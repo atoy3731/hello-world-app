972a1518dae7b0de9444e4fad887a06e13f0f95e
Adam Toy <adam.toy@rancherfederal.com>
2020-10-12 10:53:24 -0400

    kubernetes!

--- application/web.py
+++ application/web.py
@@ -10,7 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('hello_world.html', local_hostname=socket.gethostname(), local_ip=socket.gethostbyname(socket.gethostname()))
+    # return render_template('hello_world.html', local_hostname=socket.gethostname(), local_ip=socket.gethostbyname(socket.gethostname()))
+    return render_template('hello_kubernetes.html', local_hostname=socket.gethostname(), local_ip=socket.gethostbyname(socket.gethostname()))
 
 
 if __name__ == '__main__':
